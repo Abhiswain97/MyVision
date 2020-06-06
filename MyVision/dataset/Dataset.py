7216332ecf62b27c695e939c53aba3b659f46b22
Abhishek Swain <abhiswain.it.2016@gmail.com>
2020-06-06 19:11:04 +0530

    added example MNIST

--- MyVision/dataset/Dataset.py
+++ MyVision/dataset/Dataset.py
@@ -6,6 +6,8 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import albumentations as A
+import torchvision.transforms as transforms
+
 
 import operator
 
@@ -14,15 +16,9 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 class DatasetUtils:
     """
-    This class contains utilities for making a Pytorch Dataset.
-    It takes in: 
-        a pd.Dataframe, 
-        column containing image paths, 
-        column having labels,
-        train transforms,
-        validation transforms
-    & makes two datasets one for Training and the other for Validation.   
+    This class contains utilities for making a Pytorch Dataset.  
     """
+
     def __init__(
         self, train_df, image_path_column, target_column, train_tfms, valid_tfms
     ):
@@ -30,7 +26,7 @@ class DatasetUtils:
         self.image_path_column = image_path_column
         self.target_column = target_column
         self.train_tfms = train_tfms
-        self.val_tfms = valid_tfms
+        self.valid_tfms = valid_tfms
 
     def splitter(self, valid_size=0.25):
         train_images, valid_images, train_labels, valid_labels = train_test_split(
@@ -46,56 +42,61 @@ class DatasetUtils:
             valid_labels.values,
         )
 
-    @staticmethod
     def make_dataset(
-        train_df,
-        image_path_column,
-        label_column,
-        train_idx,
-        val_idx,
-        train_transforms=self.train_tfms,
-        valid_transforms=self.valid_tfms,
-        valid_size=0.25,
-        is_CV=None,
+        self, resize, train_idx=None, val_idx=None, valid_size=0.25, is_CV=None
     ):
         if is_CV:
             train_dataset = CVDataset(
                 train_df,
                 train_idx,
-                image_path_column,
-                label_column,
-                transform=train_transforms,
+                self.image_path_column,
+                self.target_column,
+                transform=self.train_tfms,
+                resize=resize,
             )
 
             valid_dataset = CVDataset(
                 train_df,
                 val_idx,
-                image_path_column,
-                label_column,
-                transform=valid_transforms,
+                self.image_path_column,
+                self.target_column,
+                transform=self.valid_tfms,
+                resize=resize,
             )
         else:
 
-            train_image_paths, train_labels, valid_image_paths, valid_labels = splitter(
-                valid_size=valid_size
+            (
+                train_image_paths,
+                train_labels,
+                valid_image_paths,
+                valid_labels,
+            ) = self.splitter(valid_size=valid_size)
+
+            train_dataset = SimpleDataset(
+                train_image_paths, train_labels, transform=self.train_tfms
             )
 
-            train_dataset = Dataset.SimpleDataset(
-                train_image_paths, train_labels, transform=train_transforms
-            )
-
-            valid_dataset = Dataset.SimpleDataset(
-                valid_image_paths, valid_labels, transform=valid_transforms
+            valid_dataset = SimpleDataset(
+                valid_image_paths, valid_labels, transform=self.valid_tfms
             )
 
         return train_dataset, valid_dataset
 
 
 class SimpleDataset(Dataset):
-    def __init__(self, image_paths, targets, transform=None):
+    def __init__(self, image_paths, targets, transform=None, resize=224):
         self.image_paths = image_paths
         self.targets = targets
-        self.defaut_tfms = A.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        self.default_tfms = transforms.Compose(
+            [
+                transforms.Resize((resize, resize)),
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                ),
+            ]
+        )
+        self.transform = transform
 
     def __getitem__(self, index: int):
         image = Image.open(self.image_paths[index])
@@ -104,7 +105,7 @@ class SimpleDataset(Dataset):
         if self.transform:
             image = self.transform(image)
 
-        image = self.defaut_tfms(image)
+        image = self.default_tfms(image)
 
         target = torch.tensor(target)
 
@@ -122,11 +123,20 @@ class CVDataset(Dataset):
         image_paths,
         target_cols,
         transform=None,
+        resize=224,
     ):
         self.df = df
         self.indices = indices
         self.transform = transform
-        self.defaut_tfms = A.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        self.default_tfms = transforms.Compose(
+            [
+                transforms.Resize((resize, resize)),
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                ),
+            ]
+        )
         self.image_paths = image_paths
         self.target_cols = target_cols
 
@@ -140,7 +150,7 @@ class CVDataset(Dataset):
         if self.transform:
             image = self.transform(image)
 
-        image = self.defaut_tfms(image)
+        image = self.default_tfms(image)
 
         return image, label
 
--- MyVision/engine/Engine.py
+++ MyVision/engine/Engine.py
@@ -1,5 +1,7 @@
 from tqdm import tqdm
 import torch
+import numpy as np
+
 from tabulate import tabulate
 
 from ..utils import Meters
@@ -8,39 +10,39 @@ from MyVision import metrics
 import os
 import time
 from itertools import chain
+import abc
 
 
-class Engine:
+class Trainer:
     def __init__(
-            self,
-            train_loader,
-            val_loader,
-            test_loader,
-            device,
-            loss,
-            optimizer,
-            model,
-            lr_scheduler,
-            accumulation_steps=1,
+        self,
+        train_loader,
+        val_loader,
+        test_loader,
+        device,
+        loss,
+        optimizer,
+        model,
+        lr_scheduler,
+        accumulation_steps=1,
     ):
         self.train_loader = train_loader
         self.val_loader = val_loader
         self.test_loader = test_loader
         self.device = device
-        self.loss = loss
+        self.criterion = loss
         self.optimizer = optimizer
         self.model = model
         self.lr_scheduler = lr_scheduler
         self.accumulation_steps = accumulation_steps
 
     def train(self):
-
         losses = Meters.AverageMeter("Loss", ":.4e")
 
         self.model.train()
 
-        tl = tqdm(self.train_loader, 1)
-        for batch_idx, (images, targets) in tl:
+        tl = tqdm(self.train_loader)
+        for batch_idx, (images, targets) in enumerate(tl, 1):
 
             self.optimizer.zero_grad()
 
@@ -49,56 +51,44 @@ class Engine:
 
             outputs = self.model(images)
 
-            loss = self.loss(outputs, targets)
-            loss.backward()
+            loss = self.criterion(outputs, targets)
 
-            losses.update(val=loss.item(), n=images.size(0))
+            loss.backward()
 
             if batch_idx % self.accumulation_steps == 0:
                 self.optimizer.step()
 
+            losses.update(val=loss.item(), n=images.size(0))
+
         return losses.avg
 
-    @torch.no_grad()
-    def valid(self):
+    def validate(self):
         losses = Meters.AverageMeter("Loss", ":.4e")
 
         self.model.eval()
 
-        predictions, gts = []
+        predictions = []
+        gts = []
 
-        vl = tqdm(self.val_loader, 1)
-        for batch_idx, (images, targets) in vl:
+        vl = tqdm(self.val_loader)
+        for batch_idx, (images, targets) in enumerate(vl, 1):
             images = images.to(self.device)
             targets = targets.to(self.device)
 
             outputs = self.model(images)
 
-            loss = self.loss(outputs, targets)
+            loss = self.criterion(outputs, targets)
+
+            predictions = chain(predictions, outputs.detach().cpu().numpy())
+            gts = chain(gts, targets.detach().cpu().numpy())
 
             losses.update(val=loss.item(), n=images.size(0))
 
-            predictions = chain(predictions, outputs.cpu().numpy())
-            gts = chain(gts, targets.detach().cpu().numpy())
-
-        return list(predictions), list(gts), losses.avg
-
-    @torch.no_grad()
-    def test(self):
-
-        self.model.eval()
-
-        predictions = []
-
-        tl = tqdm(self.test_loader, 1)
-        for batch_idx, images in tl:
-            images = images.to(self.device)
-
-            outputs = self.model(images)
-
-            predictions = chain(predictions, outputs.cpu().numpy())
-
-        return list(predictions)
+        return (
+            np.array(list(predictions)),
+            np.array(list(gts)),
+            losses.avg
+        )
 
     def fit(self, epochs, metric):
 
@@ -109,18 +99,40 @@ class Engine:
 
             train_loss = self.train()
 
-            preds, gts, valid_loss = self.valid()
+            preds, gts, valid_loss = self.validate()
 
             if valid_loss < best_loss:
 
-                if not os.path.exists(models):
-                    os.mkdir(models)
-                print('[SAVING].....')
-                torch.save(self.model.state_dict(), f'models\\best_model-({epoch}).pth.tar')
+                if not os.path.exists("models"):
+                    os.mkdir("models")
+                print("[SAVING].....")
+                torch.save(self.model.state_dict(), f"models\\best_model-({epoch}).pth.tar")
 
-            score = metrics.ClassificationMetrics()(metric, y_true=gts, y_pred=None, y_proba=preds)
+            if len(np.unique(preds)) > 2:
+
+                multiclass_metrics = ['accuracy']
+
+                preds = [np.argmax(p) for p in preds]
+
+                score = metrics.ClassificationMetrics()(
+                    metric, y_true=gts, y_pred=preds, y_proba=None
+                )
+            else:
+                binary_metrics = ['auc', 'f1', 'recall', 'precision']
+
+                preds = [1 if p >= 0.5 else 0 for p in preds]
+
+                score = metrics.ClassificationMetrics()(
+                    metric, y_true=gts, y_pred=preds, y_proba=None
+                )
+
             table_list.append((epoch, train_loss, valid_loss, score))
 
-            print(tabulate(table_list, headers=('Epoch', 'Train loss', 'Valid loss', metric)))
+            print(
+                tabulate(
+                    table_list, headers=("Epoch", "Train loss", "Validation loss", metric)
+                )
+            )
 
-            self.lr_scheduler.step(score)
+            if self.lr_scheduler:
+                self.lr_scheduler.step(score)
